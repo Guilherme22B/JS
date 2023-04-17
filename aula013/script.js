01d58fc49485b87e0d6553ba4638ccba9c8919cb
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-04-17 13:59:59 -0300

    do while

--- aula013/script.js
+++ aula013/script.js
@@ -1,7 +1,12 @@
 function rep(){
 var c = 1
 while (c <= 600){
-    document.write(`passo ${c}`)
+    document.write(`paso:${c}`)
     c++
 }
 }
+var c = 0 
+do {
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 6)
