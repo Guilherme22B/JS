3fbd9320ced30f227d5c96d85ea1b881c37cdbd0
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-04-19 11:59:27 -0300

    Update script.js

--- ex16/script.js
+++ ex16/script.js
@@ -4,7 +4,7 @@ var f = Number(document.querySelector("input#fim").value)
 var p = Number(document.querySelector("input#passo").value)
 var res = document.querySelector("div#msg")
 
-if(0 == i || 0 == f || 0 == p){
+if(null == i || 0 == f || 0 == p){
     alert("falso")
 }
 else if(null == i){
@@ -14,7 +14,6 @@ else{
 for(i = i; i <= f; i = i + p){ 
     res.innerHTML = ("contagem: ")
     console.log(i)
-
 }
 }
 }
