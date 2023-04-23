2821081af950766824d66e677e5876587a2f413f
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-04-23 19:17:52 -0300

    ex 017

--- ex17/script.js
+++ ex17/script.js
@@ -1,16 +1,43 @@
-function res(){
-    
-var n1 =  Number(document.querySelector("input").value)
-var res = document.querySelector("div#msg")
+function tabuada(){
+    var n1 =  Number(document.querySelector("input").value)
+    var res = document.querySelector("div#select > select")
 
+    if(n1 == 0){
+        alert("por favor, digite um numero!")
 
-for(c = 1; c <= 10; c++){
-    console.log(`${n1} x ${c} = ${n1 * c}`)
-    res.innerHTML += `
+        for(c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            item.text = `${n1} x ${c} = ${n1 * c}` 
+            res.appendChild(item)
 
-    <table>
-     <tr> ${n1} x ${c} = ${n1 * c} </tr>
-    </table>
-    `
 }
+
+        
+    }
+    else{
+        
+        res.innerHTML = ``
+
+
+        for(c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+            item.text = `${n1} x ${c} = ${n1 * c}` 
+            res.appendChild(item)
+
 }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*console.log(`${n1} x ${c} = ${n1 * c}`)
+res.innerHTML += `<table><tr> ${n1} x ${c} = ${n1 * c} </tr></table>`*/
