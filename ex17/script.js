5767569562cd3020e1dedf5728c8ede15912a2c0
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-04-23 18:28:36 -0300

    organizai conteudo

--- ex17/script.js
+++ ex17/script.js
@@ -6,6 +6,11 @@ var res = document.querySelector("div#msg")
 
 for(c = 1; c <= 10; c++){
     console.log(`${n1} x ${c} = ${n1 * c}`)
-    document.write(res.innerHTML)
+    res.innerHTML += `
+
+    <table>
+     <tr> ${n1} x ${c} = ${n1 * c} </tr>
+    </table>
+    `
 }
 }
