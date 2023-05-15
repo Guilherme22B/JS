708e510730c058fba430512fa79b4212ad1f394b
GuilhermeSilvaM <guimenezes125@gmail.com>
2023-05-15 14:51:31 -0300

    array dinamico

--- exFinal/script.js
+++ exFinal/script.js
@@ -1,14 +1,55 @@
-function add(){
+var memoria = []
+function adicionar(){
 
     var num = Number(document.querySelector("input").value)
+    var res = document.querySelector("div#res")
+    var selec = document.querySelector("select")
+    console.log(memoria)
 
 
-    if(num < 1 || num > 100){
+    if(num > 100 || num <= 0){
         alert("[ERRO]digite um numero entre 1 e 100")
     }
     else{
-        var memoria = []
         memoria.push(num)
-        console.log(memoria)
-    }
+        var opt = document.createElement('option')
+        opt.text = `valor ${num} adicionado`
+        selec.appendChild(opt)
+        }
 }
+
+
+function fim(){
+    res.innerHTML += `o menor numero é`
+    alert("olá")
+}
+
+
+
+
+
+
+
+
+    /*
+    var qvalor = 0 
+    var maior = 0 
+    var menor = 0 
+    var media = 0 
+    */
+
+    /*
+    if(num >= maior){
+        maior = num
+        console.log(menor)
+    }
+
+    if(num <= menor){
+        menor = num
+        console.log(menor)
+    }
+
+    if(num == num){
+        media = media + num
+    }
+    */
---
+++ test/script.js
@@ -0,0 +1,8 @@
+var pessoa = []
+
+function myfunction(){
+    var n1 = Number(document.querySelector("input#oii").value)
+
+    pessoa.push(n1)
+    console.log(pessoa)
+}
